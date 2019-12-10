0881126099b74b25d212cd38e35554bf79dbdf53
Emilly Patrick <empatric@iu.edu>
2019-12-10 11:04:38 -0500

    exception

---
+++ exceptions/data_length.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Author: Emily Patrick
+ * Date: $(DATE)
+ * File: $(FILE_NAME)
+ * Description:
+ **/
+
+class DataLength extends Exception
+{
+
+}
---
+++ exceptions/database_exception.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Author: Emily Patrick
+ * Date: $(DATE)
+ * File: $(FILE_NAME)
+ * Description:
+ **/
+
+class DatabaseException extends Exception
+{
+
+}
